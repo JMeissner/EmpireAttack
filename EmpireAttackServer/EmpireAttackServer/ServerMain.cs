75c032977ee4fe72efbe45e6df9c808365b00c19
JMeissner <meissner.julius@gmail.com>
2018-12-11 18:59:12 +0100

    1st Commit

---
+++ EmpireAttackServer/EmpireAttackServer/ServerMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpireAttackServer
+{
+    class ServerMain
+    {
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+        }
+
+        #endregion Private Methods
+    }
+}
