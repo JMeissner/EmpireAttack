2c2f53bf934f3d222e84442c2663cb41f38887f2
JMeissner <meissner.julius@gmail.com>
2019-01-08 23:19:39 +0100

    Server working now

--- EmpireAttackServer/EmpireAttackServer/ServerMain.cs
+++ EmpireAttackServer/EmpireAttackServer/ServerMain.cs
@@ -15,14 +15,16 @@ namespace EmpireAttackServer
     {
         #region Private Fields
 
+        // Configuration object
+        private static NetPeerConfiguration Config;
+
         private static Game gameInstance;
         private static Timer gameTimer;
-        private static Timer serverTimer;
 
         // Server object
         private static NetServer Server;
-        // Configuration object
-        private static NetPeerConfiguration Config;
+
+        private static Timer serverTimer;
 
         #endregion Private Fields
 
@@ -41,7 +43,7 @@ namespace EmpireAttackServer
             InitializeServer();
 
             GameTicks = 0;
-            gameInstance.Initialize(1);
+            //gameInstance.Initialize(1);
 
             //Setup Game Update Routine timer to update every 500ms
             gameTimer = new System.Timers.Timer(500);
@@ -50,10 +52,39 @@ namespace EmpireAttackServer
             gameTimer.Enabled = true;
 
             //Setup Server Update Routine timer to update every 50ms
-            serverTimer = new System.Timers.Timer(500);
+            serverTimer = new System.Timers.Timer(50);
             serverTimer.Elapsed += OnServerUpdate;
             serverTimer.AutoReset = true;
             serverTimer.Enabled = true;
+        }
+
+        private static void InitializeServer()
+        {
+            // Create new instance of configs. Parameter is "application Id". It has to be same on client and server.
+            Config = new NetPeerConfiguration("game");
+
+            // Set server port
+            Config.Port = 14242;
+
+            // Max client amount
+            Config.MaximumConnections = 200;
+
+            // Enable New messagetype. Explained later
+            Config.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
+
+            // Create new server based on the configs just defined
+            Server = new NetServer(Config);
+
+            // Start it
+            Server.Start();
+
+            // Console
+            Console.WriteLine("Server Started");
+            Console.WriteLine("---  CONFIGURATION  ---");
+            Console.WriteLine("> Type: game");
+            Console.WriteLine("> Port: 14242");
+            Console.WriteLine("> Max. Players: 200");
+            Console.WriteLine("--- /CONFIGURATION  ---");
         }
 
         /// <summary>
@@ -63,13 +94,13 @@ namespace EmpireAttackServer
         private static void Main(string[] args)
         {
             //TEST REMOVE ASAP
-            MapPNGImport map = new MapPNGImport(AppDomain.CurrentDomain.BaseDirectory + @"/Map.png");
+            //MapPNGImport map = new MapPNGImport(AppDomain.CurrentDomain.BaseDirectory + @"/Map.png");
 
             //Add Handler for application exit etc to free bound ressources
             AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
 
             //TODO: Specify args for startup -> Import Config
-            gameInstance = new Game(10);
+            //gameInstance = new Game(10);
             Initialize();
 
             Console.ReadLine();
@@ -104,41 +135,48 @@ namespace EmpireAttackServer
                         // ( Enums can be casted to bytes, so it be used to make bytes human readable )
                         if (inc.ReadByte() == (byte)PacketTypes.LOGIN)
                         {
+                            //TODO: Add Players to Custom Object/ Dictionary
+                            //TODO: Custom Timeout (Use systemtime)
+                            //TODO: Shoot answer back if msg is received
+                            inc.SenderConnection.Approve();
+                            string pName = inc.ReadString();
+                            Console.WriteLine("NEW PLAYER CONNECTION: " + pName + ", IP: " + inc.SenderConnection);
+
+                            //NetOutgoingMessage outmsg = Server.CreateMessage();
+                            //outmsg.Write((byte)PacketTypes.TEST);
+                            //outmsg.Write("Server says Hi!");
+
+                            //Server.SendMessage(outmsg, inc.SenderConnection, NetDeliveryMethod.ReliableOrdered, 0);
                         }
                         break;
+
                     case NetIncomingMessageType.Data:
 
                         if (inc.ReadByte() == (byte)PacketTypes.TEST)
                         {
+                            //TODO: Find Player Obj
+                            //TODO: Add Timer for timeout
+                            //TODO: Send response
+                            string msg = inc.ReadString();
+                            Console.WriteLine("PLAYERMSG: " + msg + ", FROM: " + inc.SenderConnection);
 
+                            NetOutgoingMessage outmsg = Server.CreateMessage();
+                            outmsg.Write((byte)PacketTypes.TEST);
+                            outmsg.Write("Server answer - " + GameTicks);
+
+                            Server.SendMessage(outmsg, Server.Connections, NetDeliveryMethod.ReliableOrdered, 0);
                         }
+                        break;
+
+                    case NetIncomingMessageType.StatusChanged:
+                        Console.WriteLine("STATUS: " + (NetConnectionStatus)inc.ReadByte() + ", FOR: " + inc.SenderConnection);
+                        break;
+
+                    default:
+                        Console.WriteLine("ERROR! --- THIS TYPE OF MESSAGE IS NOT SUPPORTED: " + inc.MessageType.ToString() + ", " + inc.ReadString());
                         break;
                 }
             }
-        }
-
-        private static void InitializeServer()
-        {
-            // Create new instance of configs. Parameter is "application Id". It has to be same on client and server.
-            Config = new NetPeerConfiguration("game");
-
-            // Set server port
-            Config.Port = 14242;
-
-            // Max client amount
-            Config.MaximumConnections = 200;
-
-            // Enable New messagetype. Explained later
-            Config.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
-
-            // Create new server based on the configs just defined
-            Server = new NetServer(Config);
-
-            // Start it
-            Server.Start();
-
-            // Console
-            Console.WriteLine("Server Started");
         }
 
         #endregion Private Methods
